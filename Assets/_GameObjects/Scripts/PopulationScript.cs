298160c39159247fc61511b96c92c69657abe1e1
beacursounity <43536093+beacursounity@users.noreply.github.com>
2019-08-04 17:51:37 +0200

    Initial commit

---
+++ Assets/_GameObjects/Scripts/PopulationScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationScript : MonoBehaviour {
+    public GameObject[] prefabs;
+    public int[] numItems;
+	void Start () {
+        float x, z;
+        float ancho = transform.localScale.x*10/2;
+        float largo = transform.localScale.z*10/2;
+        for (int i = 0; i < prefabs.Length; i++) {
+            for (int j=0; j < numItems[i]; j++) {
+                x = Random.Range(-ancho, ancho);
+                z = Random.Range(-largo, largo);
+                Instantiate(prefabs[i], new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/_GameObjects/Scripts/RemyScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemyScript : MonoBehaviour {
+    private Animator miAnimator;
+
+	void Start () {
+        miAnimator = GetComponent<Animator>();
+	}
+
+	//Version bool
+	void Update () {
+        float y = Input.GetAxis("Vertical");
+        miAnimator.SetFloat("Corriendo", y);
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            miAnimator.SetBool("EstaAndando", true);
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            miAnimator.SetBool("EstaAndando", false);
+        }
+        if (Input.GetKeyDown(KeyCode.P)) {
+            miAnimator.SetTrigger("Punch");
+        }
+        
+	}
+
+    public void ReproducirSonidoPisada() {
+        print("SONIDO DE PISADA");
+    }
+}
